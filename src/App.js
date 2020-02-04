9fb816326f104f4f7eacae1059c3d0961e82b728
Sal Tamay <sal.tamay@outlook.com>
2020-02-03 21:05:33 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import TimersDashboard from './components/TimersDashboard';
+
+function App() {
+  return (
+    <div className='App'>
+      <TimersDashboard />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TimersDashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class TimersDashboard extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1 className='main-title'>Timers</h1>
+      </div>
+    );
+  }
+}
+
+export default TimersDashboard;
