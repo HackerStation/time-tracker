ba2fe6e4cd6732195f8fa9d5556910f41e0f988e
Sal Tamay <sal.tamay@outlook.com>
2020-02-04 20:28:59 -0500

    style: Add Bootstrap Card styles to Timer component

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,17 +1,40 @@
 import React from 'react';
 import { renderElapsedString } from '../utils/utils';
+import { FaRegEdit, FaTrash } from 'react-icons/fa';
 
 class Timer extends React.Component {
+  styles = {
+    card: {
+      width: '24rem'
+    },
+    buttonGroup: {
+      display: 'flex',
+      justifyContent: 'flex-end'
+    },
+    button: {
+      border: 'none',
+      background: '#fff',
+      padding: '0'
+    }
+  };
   render() {
     const { title, project, elapsed } = this.props;
     const elapsedString = renderElapsedString(elapsed);
     return (
-      <div className='timer'>
-        <h1>{title}</h1>
-        <h3>{project}</h3>
-        <div className='timer-handle'>{elapsedString}</div>
-        <div className='timer-action'></div>
-        <button className='timer-start'>Start</button>
+      <div className='card' style={this.styles.card}>
+        <div className='card-body'>
+          <h5 className='card-title'>{title}</h5>
+          <h6 className='card-subtitle mb-2 text-muted'>{project}</h6>
+          <h1 className='card-text text-center mt-4 mb-4'>{elapsedString}</h1>
+          <div className='btn-group' style={this.styles.buttonGroup}>
+            <button className='card-link' style={this.styles.button}>
+              <FaRegEdit />
+            </button>
+            <button className='card-link' style={this.styles.button}>
+              <FaTrash />
+            </button>
+          </div>
+        </div>
       </div>
     );
   }
