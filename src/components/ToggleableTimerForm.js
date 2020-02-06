d6a54eb381040d000e8ba9c08387409303d9a94f
Sal Tamay <sal.tamay@outlook.com>
2020-02-05 23:13:59 -0500

    feat: Toogle create TimerForm when plus icon is clicked

--- src/components/ToggleableTimerForm.js
+++ src/components/ToggleableTimerForm.js
@@ -12,7 +12,10 @@ class ToggleableTimerForm extends React.Component {
     } else {
       return (
         <div className='btn-container text-center'>
-          <button className='btn btn-secondary mt-5'>
+          <button
+            className='btn btn-light mt-5'
+            onClick={() => this.setState({ isOpen: true })}
+          >
             <FaPlusSquare size={36} />
           </button>
         </div>
